f1f1191dc817e9d8fe6ac007a4d730c342c784b4
tommillerr <38590161+tommillerr@users.noreply.github.com>
2020-02-27 20:22:40 +0000

    Added Static WP HTML

--- wordpress/wp-content/plugins/easy-facebook-likebox/assets/js/espf-search-results.js
+++
@@ -1,5 +0,0 @@
-jQuery( '.espf-search-hide-btn' ).live("click", function($) {
-
-		jQuery('.plugin-card-easy-facebook-likebox-search').slideUp().remove();
-
-	 });
--- wordpress/wp-content/themes/smartparasites/searchform.php
+++
@@ -1,5 +0,0 @@
-<form role="search" method="get" class="header__search-form" action="<?php echo home_url( '/' ); ?>">
-	<label>
-		<input type="search" class="header__search-field" placeholder="SEARCH" value="" name="s" title="" />
-	</label>
-</form>
